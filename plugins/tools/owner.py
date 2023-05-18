30e8458c74dc92bde65f6b03ad7f46d8784b30ff
Indianbro67 <133229712+Indianbro67@users.noreply.github.com>
2023-05-18 16:51:35 +0530

    Update owner.py

--- plugins/tools/owner.py
+++ plugins/tools/owner.py
@@ -11,13 +11,13 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
     & ~filters.edited & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://te.legra.ph/file/f73af9a4ffe130a83d8d2.jpg",
+        photo=f"https://telegra.ph/file/19a46b9f53683efa5b46e.jpg",
         caption=f"""🥀 𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞 𝐅𝐨𝐫 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐎𝐰𝐧𝐞𝐫 🥀""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🥀 𝐎𝐰𝐧𝐞𝐫 🥀", url=f"https://t.me/BikashHalder")
+                        "🥀 𝐎𝐰𝐧𝐞𝐫 🥀", url=f"https://t.me/+FQQYOR77arQ1Njc1")
                 ]
             ]
         ),
